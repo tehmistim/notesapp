ff76048e06a27c7f2e5e1d2036064ec85b88f2b8
Tim Washington <taw@tehmwashington.com>
2021-11-29 20:07:05 -0600

    final push

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
 app.use('/stories', require('./routes/stories'))
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5500
 
 app.listen(
   PORT,
--- config/passport.js
+++ config/passport.js
@@ -9,6 +9,7 @@ module.exports = function (passport) {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: '/auth/google/callback',
+        proxy: true
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
